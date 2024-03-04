fc1af62bbd92b730d2be1656b957d87dc2324fa2
Thomas JEU <jeuthomas03@gmail.com>
2024-03-04 19:54:53 +0100

    Analytics Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './style/global.scss'
 import { createRoot } from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
     <BrowserRouter>
         <SpeedInsights />
+        <Analytics />
         <App />
     </BrowserRouter>
 );
