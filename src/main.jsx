26b7a803413d33d111b787d0fba08777fc4e9a97
Thomas JEU <70963286+thomasjeu03@users.noreply.github.com>
2024-10-21 11:24:26 +0200

    @vercel/analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import App from './App'
 import './style/global.scss'
 import { createRoot } from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
     <BrowserRouter>
         <App />
+        <Analytics />
     </BrowserRouter>
 );
